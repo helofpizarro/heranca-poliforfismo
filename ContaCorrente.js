518749bad1f43bee981025678751b97e06876620
Maria Heloisa <heloisa.fpizarro@gmail.com>
2021-01-23 17:29:44 -0300

    herança

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,10 +2,9 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
+    
    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-   _saldo = 0;
+   
     
 
 
@@ -26,6 +25,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0
         ContaCorrente.numeroDeContas += 1;
     }
 
@@ -38,7 +38,7 @@ export class ContaCorrente{
     }
 
     depositar(valor){
-        if(valor <= 0)
+        if(valor <= 100)
         {
             return;
         } 
